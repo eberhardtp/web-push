875324db3fa66a31b40a1a04c5b555dadc9e775a
eberhardtp <peter.eberhardt@pilar.hu>
2018-01-25 13:53:04 +0100

    SecureRandom -> Random

--- src/main/scala/com.zivver.webpush/Encryption.scala
+++ src/main/scala/com.zivver.webpush/Encryption.scala
@@ -1,6 +1,6 @@
 package com.zivver.webpush
 
-import java.security.{KeyPairGenerator, PublicKey, SecureRandom}
+import java.security.{KeyPairGenerator, PublicKey}
 
 import org.bouncycastle.jce.ECNamedCurveTable
 import org.bouncycastle.jce.provider.BouncyCastleProvider
@@ -13,10 +13,12 @@ object Encryption {
   private val localCurve = KeyPairGenerator.getInstance("ECDH", BouncyCastleProvider.PROVIDER_NAME)
   localCurve.initialize(ECNamedCurveTable.getParameterSpec("prime256v1"))
 
+  private def seedBytes(size: Int) = Array.fill(size)((scala.util.Random.nextInt(256) - 128).toByte)
+
   def encrypt(buffer: Array[Byte], userPublicKey: PublicKey, userAuth: Array[Byte]): Encrypted = {
     val serverKeys = localCurve.generateKeyPair
     //noinspection ScalaStyle
-    val salt = SecureRandom.getSeed(16)
+    val salt = seedBytes(16)
     val ciphertext = HttpEce.encrypt(serverKeys, buffer, salt, userPublicKey, userAuth)
     Encrypted(serverKeys.getPublic, salt, ciphertext)
   }
